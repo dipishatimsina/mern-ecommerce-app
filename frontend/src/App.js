b816e99305c535ea47c01094a64eeb05622b0881
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-04 13:46:18 +0545

    add Home page and configure React Router with RouterProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="text-red-600">
-      MERN app
+    <div>
+      <Outlet />
     </div>
   );
 }
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-gray-800">Home</h1>
+    </div>
+  )
+}
+
+export default Home;
