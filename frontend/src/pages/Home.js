26c0abb75883a31953f1e62fcb19a9664b508733
dipishatimsina <dipishatimsina1@gmail.com>
2026-06-05 12:23:09 +0545

    update Home page with Tailwind styling

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,9 +1,9 @@
+import React from 'react'
+
 const Home = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-3xl font-bold text-gray-800">Home</h1>
-    </div>
+    <div>Home</div>
   )
 }
 
-export default Home;
+export default Home
